84dde86b52476911b9a92cc912ed39ab09653505
mlucas94 <lucasmoron0512@gmail.com>
2020-11-26 02:01:06 -0300

    Se empieza a trabajar logica del juego. Instrucciones basicas en README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # unq-ui-lucas-moron-trabajo-final
+
+
+## Instalacion
+
+Dentro de la carpeta integrador-ui, ejecutar en consola npm install
+
+## Ejecucion
+
+Dev mode: Dentro de la carpeta integrador-ui, ejecutar en consola npm start
--- integrador-ui/src/components/Game.jsx
+++ integrador-ui/src/components/Game.jsx
@@ -13,6 +13,40 @@ import React from 'react'
     popup/alerta que muestre quien gano y pregunte si se quiere continuar jugando
  */
 
+const [player1, setPlayer1] = useState({
+    choice = null,
+    wins = 0
+  })
+  const [player2, setPlayer2] = useState({
+    playerChoice = null,
+    wins = 0
+  })
+  const [gameData, setGameData] = useState({
+      gamesPlayed = 0,
+      winner = null,
+      options = ["rock", "paper", "scissors", "lizard", "Spock"]
+  })
+
+  const winner = () => {
+      if(player1.playerChoice == player2.playerChoice) {
+          return "Draw"
+      }
+      if (player1Victory) {
+        return "Player 1"
+      } else {
+          return "Player 2"
+      }
+  }
+
+  const player1Victory = () => {
+    return (player1.choice  == "rock" && (player2.choice == "scissors" || player2.choice == "lizard"))
+    || (player1.choice == "paper" && (player2.choice == "rock" || player2.choice == "Spock"))
+    || (player1.choice == "scissors" && (player2.choice == "paper" || player2.choice == "lizard"))
+    || (player1.choice == "lizard" && (player2.choice == "Spock" || player2.choice == "paper"))
+    || (player1.choice == "Spock" && (player2.choice == "rock") || player2.choice == "scissors") 
+  }
+   
+
 const Game = () => {
     return (<h1>Game page</h1> )
 }
